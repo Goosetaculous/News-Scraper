970cceed1ddeedfabc25a9066c110ab9385e0809
Goose <josephtrop@gmail.com>
2017-08-26 00:46:09 -0700

    change package.json for deployment

--- config/config.js
+++ config/config.js
@@ -8,4 +8,6 @@ if(env ==='development'){
 }else if(env === 'test'){
     process.env.PORT=3000
     process.env.MONGODB_URI = 'mongodb://josephtrop:P)o9I*u7@ds129402.mlab.com:29402/goose'
+}else {
+    process.env.MONGODB_URI = 'mongodb://josephtrop:P)o9I*u7@ds159493.mlab.com:59493/heroku_j5vkz9g7'
 }
